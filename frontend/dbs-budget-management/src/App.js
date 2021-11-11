0167769e358df75fb55b67dc17276f38f7f2a9f9
Menglin <huang.menglin@dhs.sg>
2021-11-11 14:23:26 +0800

    created dashboard component

--- frontend/dbs-budget-management/src/App.js
+++ frontend/dbs-budget-management/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './components/Login'
+import Dashboard from './components/Dashboard'
 
 function App() {
   return (
     <div>
       <Login />
+      <Dashboard />
     </div>
   );
 }
---
+++ frontend/dbs-budget-management/src/components/Dashboard.js
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+import './Dashboard.css';
+
+const Dashboard = () => {
+    const [currUserId] = useState(1)
+    const [projects] = useState([
+        {
+            "id": 1,
+            "user_id": 4,
+            "name": "RTF",
+            "budget": 12000,
+            "description": "Realtime Face Recogniton"
+        },
+        {
+            "id": 2,
+            "user_id": 1,
+            "name": "SWT",
+            "budget": 80000,
+            "description": "Smart Watch Tracker"
+        },
+        {
+            "id": 2,
+            "user_id": 1,
+            "name": "SWT",
+            "budget": 80000,
+            "description": "Smart Watch Tracker"
+        },
+        {
+            "id": 2,
+            "user_id": 1,
+            "name": "SWT",
+            "budget": 80000,
+            "description": "Smart Watch Tracker"
+        },
+        {
+            "id": 2,
+            "user_id": 1,
+            "name": "SWT",
+            "budget": 80000,
+            "description": "Smart Watch Tracker"
+        },
+        {
+            "id": 2,
+            "user_id": 1,
+            "name": "SWT",
+            "budget": 80000,
+            "description": "Smart Watch Tracker"
+        },
+        {
+            "id": 3,
+            "user_id": 2,
+            "name": "ULS",
+            "budget": 11000,
+            "description": "Upgrade Legacy System"
+        }
+    ])
+
+
+    return (
+        <div className='dashboard'>
+            <h2>My Project Dashboard</h2>
+            <div className='row'>
+                {projects.filter((project) => (project.user_id === currUserId)).map((project) => (
+                    <div className='column'>
+                        <div key={project.id} className="card">
+                            <div className="container">
+                                <h4><b>{project.name}</b></h4>
+                                <p><b>Project Description: </b>{project.description}</p>
+                                <p><b>Project Budget: </b>${project.budget}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+        </div>
+    )
+}
+
+export default Dashboard
