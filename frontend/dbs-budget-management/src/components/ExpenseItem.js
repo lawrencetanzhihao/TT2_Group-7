d50376b711ccbdd82d110d74de377280d94bab1d
Lawrence Tan <lawrencetanzhihao@gmail.com>
2021-11-11 13:24:30 +0800

    added update and delete button in ExpenseItem

--- frontend/dbs-budget-management/src/components/ExpenseItem.js
+++ frontend/dbs-budget-management/src/components/ExpenseItem.js
@@ -1,12 +1,39 @@
 
 
-import React from 'react'
+import React from 'react';
+import { MdEdit, MdCancel } from 'react-icons/md';
 
-const ExpenseItem = () => {
+const ExpenseItem = ({ expense }) => {
+
+   const { id, description, amount } = expense;
+
    return (
-      <div>
-         
-      </div>
+      <>
+         <li className="item">
+            <div className="info">
+               <span className="expense">{description}</span>
+               <span className="amount">${amount}</span>
+            </div>
+
+            <div>
+               <button
+                  className="edit-btn"
+                  aria-label="update button"
+               >
+                  <MdEdit />
+               </button>
+
+               <button
+                  className="edit-btn"
+                  aria-label="update button"
+               >
+                  <MdCancel />
+               </button>
+            </div>
+
+
+         </li>
+      </>
    )
 }
 
