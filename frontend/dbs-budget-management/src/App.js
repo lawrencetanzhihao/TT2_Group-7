1f53eee37b3e0cc7c5c013fb460c2f03a3b4f2b8
Lawrence Tan <lawrencetanzhihao@gmail.com>
2021-11-11 12:29:00 +0800

    created header div

--- frontend/dbs-budget-management/src/App.js
+++ frontend/dbs-budget-management/src/App.js
@@ -1,16 +1,35 @@
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Alert from './components/Alert';
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from './components/ExpenseList';
 
+import { MdAttachMoney } from 'react-icons/md';
+
 function App() {
+
+
+  //******** Initial States ********//
+  const [expenses, setExpenses] = useState([]);
+
+
   return (
-    <div className="App">
-      <ExpenseForm />
-      <ExpenseList />
-      <Alert />
-    </div>
+    <>
+      <main className="App">
+
+        <div className="header-container">
+          <div className="header-title">
+            <MdAttachMoney />
+            Project Expense Tracker
+          </div>
+        </div>
+
+
+        <ExpenseForm />
+        <ExpenseList />
+        <Alert />
+      </main>
+    </>
   );
 }
 
